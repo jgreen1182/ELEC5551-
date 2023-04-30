0f8ac5e65bcf35cbc1b2800ecd0321684cbb0ed0
jgreen1182 <103915420+jgreen1182@users.noreply.github.com>
2023-04-30 18:13:22 +0800

    Create BMS_CODE_(2_batteries)_REV_B.ino
    2nd revision of the BMS code
    
    Changes made:
    1. Added function for reading temperature from LM35 sensors
    2. Extended SD card writing function to add headers appropriately for operational batteries
    3. Extended Main loop code to write temperature values for operational batteries to the SD card .txt file

---
+++ BMS_CODE_(2_batteries)_REV_B.ino
@@ -0,0 +1,209 @@
+#include <SPI.h>
+#include <SD.h>
+
+const int switchPin = 6; // Digital pin for the switch controlling the motor
+
+//DEFINE SAFE OPERATING LIMITS
+const float voltageMin = 8;
+const float voltageMax = 9;
+
+//DEFINE PIN CONNECTIONS
+const int voltagePins[] = {A0, A1}; //Analog pins for taking voltage readings
+const int tempPins[] = {A4, A5}; //Analog pins for taking temperature readings
+const int greenLEDs[] = {2, 3}; // GREEN LEDs for batteries
+const int redLEDs[] = {4, 5}; // RED LEDs for batteries
+const int transistorPins[] = {8, 9}; // Transistor base pins for hooking batteries to the rest of the circuit
+
+//DEFINE VOLTAGE DIVIDER RATIO
+const float R1 = 10000.0; // R1 resistor value in ohms (change to your actual value)
+const float R2 = 10000.0; // R2 resistor value in ohms (change to your actual value)
+
+//FOR COUNTING CONNECTED BATTERIES
+const int initialNumBatteries = 2;
+int numBatteries = 0;
+
+//FOR SD CARD
+String fileName;
+const int chipSelect = 10; //chip select for the SD card
+bool headerWritten = false; //To check if header has already been written to SD card
+
+//Structure for storing information on each battery
+struct BatteryInfo{
+  int voltagePin;
+  int tempPin;
+  int greenLED;
+  int redLED;
+  int transistorPin;
+};
+
+//Creates an array of battery info struct named operational batteries with a length equal to the initial number of batteries
+BatteryInfo operationalBatteries[initialNumBatteries];
+
+//FUNCTION FOR READING VOLTAGE VALUES
+float readBatteryVoltage(int pin){
+  int analogVoltage = analogRead(pin);
+  float voltage = (analogVoltage/ 1023.0) * 5.0; //Convert the reading to voltage (assuming a 5V reference)
+
+  // Apply voltage divider ratio
+  float voltageDividerRatio = (R1 + R2) / R2;
+  voltage *= voltageDividerRatio;
+
+  return voltage;
+}
+
+//FUNCTION FOR READING TEMPERATURE VALUES
+float readBatteryTemperature(int pin){
+  int analogTemp = analogRead(pin);
+  float tempVoltage = (analogTemp / 1023.0) * 5.0; //Convert the reading to voltage (assuming a 5V reference)
+  float temperature = tempVoltage * 100.0; // Convert voltage to temperature in Celsius (for LM35)
+
+  return temperature;
+}
+
+//FUNCTION FOR WRITING DATA TO THE SD CARD
+void writeDataToSDCard(String dataString) {
+  //OPEN THE FILE
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    // Write header row if the file is empty and the header row has not been written yet
+    if (dataFile.size() == 0 && !headerWritten) {
+      dataFile.print("Timestamp,");
+
+      //Add voltage headers
+      for (int i = 0; i < numBatteries; i++) {
+        int batteryIndex = operationalBatteries[i].voltagePin - A0 + 1;
+        dataFile.print("Battery " + String(batteryIndex) + " (V),");
+        if (i < numBatteries - 1) {
+          dataFile.print(",");
+        }
+      }
+
+      //add temperature headers
+      for (int j = 0; j < numBatteries; j++) {
+        int batteryIndex = operationalBatteries[j].voltagePin - A0 + 1;
+        dataFile.print("Battery " + String(batteryIndex) + " (°C)");
+        if (j < numBatteries - 1) {
+          dataFile.print(",");
+        }
+      } 
+
+      dataFile.println();
+      headerWritten = true;
+    }
+
+    // Write data row
+    dataFile.print(millis());
+    dataFile.print(",");
+    dataFile.println(dataString);
+
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening " + fileName);
+  }
+}
+
+//FUNCTION FOR GENERATING SD CARD FILE NAME
+String generateIndexedFileName() {
+  int fileIndex = 0;
+  String baseName = "datalog";
+  String fileExtension = ".txt";
+  
+  while (SD.exists(baseName + String(fileIndex) + fileExtension)) {
+    fileIndex++;
+  }
+  
+  return baseName + String(fileIndex) + fileExtension;
+}
+
+//MAIN SETUP
+void setup() {
+
+  pinMode(switchPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  //pinMode(chipSelect, OUTPUT);
+
+  while(!Serial){
+    ; // wait for serial port to connect. needed for native USB port only
+  }
+
+  Serial.print("Initialising SD card...");
+
+  if(!SD.begin(chipSelect)){
+    Serial.println("initialization failed!");
+    while(1);
+  }
+  Serial.println("initialization done.");
+
+  fileName = generateIndexedFileName();
+  Serial.println("Created new file: " + fileName);
+
+  // Check battery voltages and turn on respective LEDs if within safe operating range GREEN and connect to circuit, if not within safe operating range RED and do not connect to circuit
+  for (int i = 0; i < initialNumBatteries; i++) {
+    pinMode(greenLEDs[i], OUTPUT);
+    pinMode(redLEDs[i], OUTPUT);
+    pinMode(transistorPins[i], OUTPUT);
+
+    float voltage = readBatteryVoltage(voltagePins[i]);
+    if (voltage >= voltageMin && voltage <= voltageMax) {
+      digitalWrite(greenLEDs[i], HIGH);
+      digitalWrite(redLEDs[i], LOW);
+      digitalWrite(transistorPins[i], HIGH); // Turn on the transistor
+
+      // Add battery to the list of operational batteries
+      BatteryInfo batteryInfo = {voltagePins[i], tempPins[i], greenLEDs[i], redLEDs[i], transistorPins[i]};
+      operationalBatteries[numBatteries] = batteryInfo;
+      numBatteries++;
+    } else {
+      digitalWrite(greenLEDs[i], LOW);
+      digitalWrite(redLEDs[i], HIGH);
+      digitalWrite(transistorPins[i], LOW); // Turn off the transistor
+      Serial.println("Battery " + String(i + 1) + " is faulty.");
+    }
+  }
+}
+
+
+//MAIN LOOP
+void loop() {
+  // Wait for the switch to be activated
+  while (digitalRead(switchPin) == LOW) {
+    delay(100);
+  }
+
+  //MAKE A STRING FOR ASSEMBLING THE DATA TO LOG
+  String dataString="";
+
+  //read the voltage values and append to the string
+  float totalVoltage = 0;
+  for(int i = 0; i < numBatteries; i++){
+    float voltage = readBatteryVoltage(operationalBatteries[i].voltagePin);
+    dataString += String(voltage);
+    if (i < numBatteries - 1) {
+      dataString += ",";
+    }
+  }
+
+  // Add a comma separator between voltage and temperature readings
+  dataString += ",";
+
+  //read the temperature values and append to the string
+  for(int i = 0; i < numBatteries; i++){
+    float temperature = readBatteryTemperature(operationalBatteries[i].tempPin);
+    dataString += String(temperature);
+    if (i < numBatteries - 1) {
+    dataString += ",";
+    }
+  }
+
+  // Call function to write data to SD card
+  writeDataToSDCard(dataString);
+
+  // Wait for 1 second before the next iteration
+  delay(1000);
+}
